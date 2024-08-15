83ae1f7f5bab9254b066ef8148cd7f4e35e0ff34
kc-clintone <kayseeclintone@gmail.com>
2024-08-15 12:53:40 +0000

    Task 2: Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,12 +4,13 @@ Working with CSV files
 """
 
 
-from typing import List
 import csv
+import math
+from typing import List, Dict, Any
 
 
 def index_range(page: int, page_size: int) -> tuple:
-    """
+   """
     Takes two integer arguments page and page_size.
     """
     start_index = (page - 1) * page_size
@@ -36,10 +37,10 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        """
-        takes two integer arguments page with default value 1
-        and page_size with default value 10.
-        """
+    """
+    Takes two integer arguments page with default value 1 and
+    page_size with default value 10.
+    """
         assert isinstance(page, int) and page > 0, "Page must be an integer greater than 0"
         assert isinstance(page_size, int) and page_size > 0, "Page size must be an integer greater than 0"
 
@@ -51,3 +52,24 @@ class Server:
             return []
 
         return dataset[start_index:end_index]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict[str, Any]:
+    """
+    Takes the same arguments (and defaults) as get_page and returns a
+    dictionary containing the following key-value pairs:
+    """
+        data = self.get_page(page, page_size)
+        total_data_items = len(self.dataset())
+        total_pages = math.ceil(total_data_items / page_size)
+
+        next_page = page + 1 if page < total_pages else None
+        prev_page = page - 1 if page > 1 else None
+
+        return {
+            "page_size": len(data),
+            "page": page,
+            "data": data,
+            "next_page": next_page,
+            "prev_page": prev_page,
+            "total_pages": total_pages,
+        }
