095e07ac9f8d823edb1103a1fc7c144c00cff96d
kc-clintone <kayseeclintone@gmail.com>
2024-08-21 21:25:48 +0000

    Task 4: MRU caching

--- 0x01-caching/4-mru_cache.py
+++ 0x01-caching/4-mru_cache.py
@@ -15,27 +15,43 @@ class MRUCache(BaseCaching):
 
     def __init__(self):
         """
-        Initialize the cache.
+        Initializing the cache.
         """
         super().__init__()
-        self.cache_data = OrderedDict()
+        self.access_order = []
 
     def put(self, key, item):
         """
         Adding an item in the cache with MRU replacement policy.
         """
-        if key is not None and item is not None:
-            if key in self.cache_data:
-                del self.cache_data[key]
+        if key is None or item is None:
+            return
 
-            self.cache_data[key] = item
+        if key in self.cache_data:
+            self.access_order.remove(key)
+        elif len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            mru_key = self.access_order.pop()
+            del self.cache_data[mru_key]
+            print(f"DISCARD: {mru_key}")
 
-            if len(self.cache_data) > BaseCaching.MAX_ITEMS:
-                discarded_key, _ = self.cache_data.popitem(last=True)
-                print(f"DISCARD: {discarded_key}")
+        self.cache_data[key] = item
+        self.access_order.append(key)
 
     def get(self, key):
         """
         Getting an item using key.
         """
-        return self.cache_data.get(key, None)
+        if key is None or key not in self.cache_data:
+            return None
+
+        self.access_order.remove(key)
+        self.access_order.append(key)
+        return self.cache_data[key]
+
+    def print_cache(self):
+        """
+        Prints the cache content.
+        """
+        print("Current cache:")
+        for key in sorted(self.cache_data.keys()):
+            print(f"{key}: {self.cache_data[key]}")
