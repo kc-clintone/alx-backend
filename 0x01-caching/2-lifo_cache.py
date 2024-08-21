5423975140746c83c95b06b7321c7e6822dac012
kc-clintone <kayseeclintone@gmail.com>
2024-08-21 20:55:44 +0000

    Task 2: LIFO caching

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python3
+"""
+LIFO caching
+"""
+
+from base_caching import BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    """
+    LIFOCache implements a caching system using
+    LIFO (Last-In, First-Out) algorithm.
+    """
+
+    def __init__(self):
+        """
+        Initializing the cache.
+        """
+        super().__init__()
+        self.last_key = None
+
+    def put(self, key, item):
+        """
+        Adding an item in the cache with LIFO replacement policy.
+        """
+        if key is not None and item is not None:
+            if key not in self.cache_data and len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+
+                if self.last_key:
+                    del self.cache_data[self.last_key]
+                    print(f"DISCARD: {self.last_key}")
+
+            self.cache_data[key] = item
+            self.last_key = key
+
+    def get(self, key):
+        """
+        Getting an item using key.
+        """
+        return self.cache_data.get(key, None)
