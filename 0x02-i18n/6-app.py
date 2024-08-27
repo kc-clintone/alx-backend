8615dd9cbbf28d8170b9d3a4c258a19b216c9a76
kc-clintone <kayseeclintone@gmail.com>
2024-08-27 13:35:04 +0000

    Task 6: Use user locale

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -28,6 +28,7 @@ app.config.from_object(Config)
 app.url_map.strict_slashes = False
 babel = Babel(app)
 
+
 users = {
     1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
     2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
