e412492802b5f3702904ca41b093d8bba6147d58
kc-clintone <kayseeclintone@gmail.com>
2024-08-27 11:45:00 +0000

    Task 0: Basic flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+"""
+0-app.py - A basic Flask application.
+
+This app sets up a simple web server with a single route '/'.
+When accessed, the route renders an HTML page with the title
+"Welcome to Holberton"
+and a header "Hello world".
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    """
+    The index view function that handles requests to the
+    root URL ('/').
+
+    This function returns the rendered HTML template
+    'index.html', which
+    displays a page with the title "Welcome to Holberton" and
+    a header "Hello world".
+    """
+    return render_template('0-index.html')
